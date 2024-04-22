9b213cc432d905da41ee7b56428a19204517cfee
Varun Chaubey <varunchaubey4@gmail.com>
2024-04-22 10:42:37 +0530

    Creating data query service and parsing incoming events

--- comments/index.js
+++ comments/index.js
@@ -19,7 +19,6 @@ app.post('/posts/:id/comments', async (req, res) => {
     comments.push({ id: commentId, content });
     commentsByPostId[req.params.id] = comments;
 
-
     try {
         await axios.post('http://localhost:4005/events', {
         type: 'CommentCreated',
--- posts/index.js
+++ posts/index.js
@@ -22,7 +22,7 @@ app.post("/posts", async (req, res) => {
 
   try {
     await axios.post('http://localhost:4005/events', {
-    type: 'postCreated',
+    type: 'PostCreated',
     data: {
       id, title
     }
